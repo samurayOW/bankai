609d892659a7a6443953832d988c560c52435a6d
samurayOW <edicmichailov@gmail.com>
2024-11-16 18:36:58 +0200

    Create strapi for new arrivals section

--- src/components/newArrivals/NewArrivals.jsx
+++ src/components/newArrivals/NewArrivals.jsx
@@ -1,8 +1,22 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
 import "./newArrivals.css";
 import NewArrivalsItem from "../newArrivalsItem/NewArrivalsItem";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../strapi";
 
 function NewArrivals() {
+  const [mangas, setMangas] = useState([]);
+
+  useEffect(function () {
+    async function fetchMangas() {
+      const res = await fetch(`${BASE_URL}/api/mangas?populate=*`);
+      const data = await res.json();
+      data.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+      setMangas(data.data.slice(0, 3));
+    }
+    fetchMangas();
+  }, []);
+
   return (
     <section className="bankai__section-new-arrivals section__padding">
       <div className="bankai__new-arrivals-inner">
@@ -16,9 +30,17 @@ function NewArrivals() {
           </div>
         </div>
         <ul className="bankai__new-arrivals-list">
+          {/* <NewArrivalsItem />
           <NewArrivalsItem />
-          <NewArrivalsItem />
-          <NewArrivalsItem />
+          <NewArrivalsItem /> */}
+          {mangas.map((manga) => (
+            <NewArrivalsItem
+              title={manga.Title}
+              cover={manga.Cover.url}
+              price={manga.Price}
+              key={manga.id}
+            />
+          ))}
         </ul>
       </div>
     </section>
