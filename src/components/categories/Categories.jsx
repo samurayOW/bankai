ff6b8698637d96f41e49122d69ad77e6f4c76c88
samurayOW <edicmichailov@gmail.com>
2024-11-29 21:30:20 +0200

    Add loaders to homepage

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -2,6 +2,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import "./categories.css";
 import { BASE_URL } from "../../strapi";
 import { useEffect, useState } from "react";
+import { MoonLoader } from "react-spinners";
 
 function Category({ title, cover }) {
   return (
@@ -14,12 +15,15 @@ function Category({ title, cover }) {
 
 function Categories() {
   const [genres, setGenres] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function fetchGenres() {
+      setIsLoading(true);
       const res = await fetch(`${BASE_URL}/api/genres?populate=*`);
       const data = await res.json();
       setGenres(data.data.slice(0, 3));
+      setIsLoading(false);
     }
     fetchGenres();
   }, []);
@@ -33,13 +37,17 @@ function Categories() {
         </div>
         <div className="bankai__categories-center">
           <ul className="bankai__categories-list">
-            {genres.map((genre) => (
-              <Category
-                title={genre.Title}
-                cover={genre.Cover.url}
-                key={genre.id}
-              />
-            ))}
+            {isLoading ? (
+              <MoonLoader color="#000" speedMultiplier={0.25} />
+            ) : (
+              genres.map((genre) => (
+                <Category
+                  title={genre.Title}
+                  cover={genre.Cover.url}
+                  key={genre.id}
+                />
+              ))
+            )}
           </ul>
           <p>Discover your next favorite story: browse our genres</p>
           <div className="bankai__categories-btn">
