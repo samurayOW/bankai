1e180ea9590f54c6ebb0f5941dc0f3343be7f751
samurayOW <edicmichailov@gmail.com>
2024-12-01 14:06:34 +0200

    Create categories page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,62 @@
 import Navbar from "../components/navbar/Navbar";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { BASE_URL } from "../strapi";
+import { useEffect, useState } from "react";
+import { MoonLoader } from "react-spinners";
+import { Link } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+
+function Category({ title, cover }) {
+  return (
+    <li className="bankai__categories-list_item page__category-item">
+      <img src={`${BASE_URL}${cover}`} alt="category" />
+      <h4>{title}</h4>
+    </li>
+  );
+}
 
 function Categories() {
+  const [genres, setGenres] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchGenres() {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/api/genres?populate=*`);
+      const data = await res.json();
+      setGenres(data.data);
+      setIsLoading(false);
+    }
+    fetchGenres();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div>This is categories page</div>
+      <section className="bankai__section-categories">
+        <div className="bankai__categories-inner">
+          <div className="bankai__categories-heading">
+            <h1>All categories</h1>
+            <p>Find what you are looking for</p>
+          </div>
+          <div className="bankai__categories-center">
+            <ul className="bankai__categories-list">
+              {isLoading ? (
+                <MoonLoader color="#000" speedMultiplier={0.25} />
+              ) : (
+                genres.map((genre) => (
+                  <Category
+                    title={genre.Title}
+                    cover={genre.Cover.url}
+                    key={genre.id}
+                  />
+                ))
+              )}
+            </ul>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 }
